a25f4a2e61be38e8fda35fc2173e81c85b04e505
James <jameslimbj@gmail.com>
2025-06-02 16:39:24 +0800

    create scratch on current buffer rather than splitting

--- nvim/.config/nvim/lua/custom/scratch.lua
+++ nvim/.config/nvim/lua/custom/scratch.lua
@@ -1,18 +1,12 @@
 local scratch_id = 0
 
-local function scratch_split(split_cmd)
-  return function()
-    scratch_id = scratch_id + 1
-    vim.cmd(split_cmd)
-    vim.bo.buftype = 'nofile'
-    vim.bo.bufhidden = 'wipe'
-    vim.bo.swapfile = false
-    vim.cmd('file [Scratch ' .. scratch_id .. ']')
-  end
+local function scratch_buffer()
+  scratch_id = scratch_id + 1
+  vim.cmd('enew')
+  vim.bo.buftype = 'nofile'
+  vim.bo.bufhidden = 'wipe'
+  vim.bo.swapfile = false
+  vim.cmd('file [Scratch ' .. scratch_id .. ']')
 end
 
-vim.keymap.set('n', '<C-w>s', scratch_split('new'),  { desc = 'Scratch horizontal split' })
-vim.keymap.set('n', '<C-w>S', scratch_split('new'),  { desc = 'Scratch horizontal split' })
-vim.keymap.set('n', '<C-w>v', scratch_split('vnew'), { desc = 'Scratch vertical split' })
-vim.keymap.set('n', '<C-w>V', scratch_split('vnew'), { desc = 'Scratch vertical split' })
-
+vim.keymap.set('n', '<leader>ss', scratch_buffer, { desc = 'Make current buffer a scratch buffer' })
